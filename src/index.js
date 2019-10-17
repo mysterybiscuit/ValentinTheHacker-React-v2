2d28d2bc1221049b3270d2f923842b8aa7cc2fe5
mysterybiscuit <guest91111@yandex.com>
2019-10-17 04:42:59 +0200

    Added all the packages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <Router />;
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
